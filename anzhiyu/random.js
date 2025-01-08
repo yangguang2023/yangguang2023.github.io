485c873a8ac5a601e2b3719510a5888b5bd592ed
yangguang2023 <397536255@qq.com>
2025-01-08 09:04:02 +0800

    Site updated: 2025-01-08 09:04:01

--- anzhiyu/random.js
+++
@@ -1,3 +0,0 @@
-var posts=["2025/01/05/hello-world/","2025/01/05/第一篇博客/","2025/01/05/如何开启慧悟/","2025/01/05/道德与因果链(上)/"];function toRandomPost(){
-    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
-  };
